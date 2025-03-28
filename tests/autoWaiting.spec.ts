b0f30482bcfbe3e99247f45059d6e74f95ec7447
viragz158 <zaveri.v@northeasatern.edu>
2025-03-28 00:41:31 -0400

    Add more example of auto waits

--- tests/autoWaiting.spec.ts
+++ tests/autoWaiting.spec.ts
@@ -5,7 +5,7 @@ test.beforeEach(async({page}) => {
     await page.getByText('Button Triggering AJAX Request').click()
 })
 
-test('Auto wait test', async ({ page }) => {
+test('Auto wait test using expect', async ({ page }) => {
 
     const successMsg = page.locator('.bg-success')
     const btnText = await successMsg.textContent()
@@ -14,3 +14,23 @@ test('Auto wait test', async ({ page }) => {
   await expect(btnText).toEqual('Data loaded with AJAX get request.')
 
 });
+
+test('Wait with state', async ({ page }) => {
+    
+    // Waits for specific element state
+    await page.locator('.bg-success').waitFor({ state: 'visible' });
+    const text = await page.locator('.bg-success').textContent();
+    expect(text).toBe('Data loaded with AJAX get request.');
+  });
+
+  test('Wait with custom timeout', async ({ page }) => {
+    // Sets custom timeout for the assertion
+    await expect(page.locator('.bg-success')).toHaveText('Data loaded with AJAX get request.', { timeout: 16000 });
+  });
+
+  test('Wait using locator', async ({ page }) => {
+
+    // Automatically waits for element to be visible
+    const text = await page.locator('.bg-success').textContent();
+    expect(text).toBe('Data loaded with AJAX get request.');
+  });
